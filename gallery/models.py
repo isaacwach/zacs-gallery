4b127329c7de711b57417ae0e31dc2def4b14b98
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-25 09:59:39 +0300

    added a location class in gallery.models

--- gallery/models.py
+++ gallery/models.py
@@ -63,3 +63,24 @@ class Category(models.Model):
 
     def delete_category(self):
         self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+   
+    def __str__(self):
+       return self.name
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def get_location(cls):
+        location = Location.objects.all()
+        return location    
+    
+    @classmethod
+    def upt_location(cls, id, value):
+        cls.objects.filter(id=id).update(image=value)
+    
+    def del_location(self):
+        self.delete()
