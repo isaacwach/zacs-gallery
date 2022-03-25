45739370c8a64e0df03eada4d09a797af70f8a2a
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-25 15:40:42 +0300

    added postgres database

--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'personal_gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'postgres',
+        'PASSWORD': ''
     }
 }
 
