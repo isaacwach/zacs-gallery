97f4b4c6298083ebd8adfeab79412222b8e7a127
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-25 09:58:08 +0300

    added an a category class

--- gallery/models.py
+++ gallery/models.py
@@ -51,3 +51,15 @@ class Image(models.Model):
     def search_by_category(cls, category):
         images = cls.objects.filter(category__name__icontains=category)
         return images
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
