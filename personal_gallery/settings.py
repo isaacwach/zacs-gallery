2c2383309a898759b3467c130661bfff2885073a
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-28 11:03:28 +0300

    deployment

--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # configuring the location for media
 MEDIA_URL = '/media/'
