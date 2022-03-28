9294f738108f1c006d54321b365ec9c6d5e8abcb
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-28 10:55:38 +0300

    deployment

--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -88,6 +88,7 @@ if config('MODE')=="dev":
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT':''
     }
